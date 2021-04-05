841e2b37bf0f32a6eee979996cbd6f37d65aa377
RoeJecc <77800695+RoeJecc@users.noreply.github.com>
2021-04-05 12:18:42 -0400

    Input Fields work

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,22 @@
+// Modal 
 const openModal = document.querySelector('.profile__edit-button');
 const overlay = document.querySelector('.overlay');
 const modal = document.querySelector('.modal');
 const closeModal = document.querySelector('.modal__close-button');
-var activeLike = document.querySelector('.element__button_active');
 
+// Modal Inputs
+const profileName = document.querySelector('.profile__name');
+const profileOccupation = document.querySelector('.profile__occupation');
+
+const profileForm = document.forms.profile;
+const profileFormNameInput = profileForm.elements.name;
+const profileFormOccupationInput = profileForm.elements.occupation;
 
 openModal.addEventListener('click', function (event) {
     modal.style.display = 'flex';
     overlay.style.display = 'block';
+    profileFormNameInput.value = profileName.textContent;
+    profileFormOccupationInput.value = profileOccupation.textContent;
 });
 
 closeModal.addEventListener('click', function (event){
@@ -16,10 +25,35 @@ closeModal.addEventListener('click', function (event){
 });
 
 
+
+
+profileForm.addEventListener('submit', function (event) {
+    profileName.textContent = profileFormNameInput.value;
+    profileOccupation.textContent = profileFormOccupationInput.value;
+    modal.style.display = 'none';
+    overlay.style.display = 'none';
+    event.preventDefault();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Hearts
+var activeLike = document.querySelector('.element__button_active');
+
 activeLike.addEventListener('click', function (event) {
     if (activeLike.style.fill === 'none') {
         activeLike.style.fill = 'black';
     } else if (activeLike.style.fill ='black') {
         activeLike.style.fill = 'none';
-    }
+    } return;
 });
