66eb67c787d5b442b98e3e1696a52a0fa0bc134d
RoeJecc <77800695+RoeJecc@users.noreply.github.com>
2021-04-06 14:44:17 -0400

    first review changes (sans javascript)

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 // Modal 
 const openModal = document.querySelector('.profile__edit-button');
-const overlay = document.querySelector('.overlay');
+const popup = document.querySelector('.popup');
 const modal = document.querySelector('.modal');
 const closeModal = document.querySelector('.modal__close-button');
 
@@ -14,21 +14,21 @@ const profileFormOccupationInput = profileForm.elements.occupation;
 
 openModal.addEventListener('click', function (event) {
     modal.style.display = 'flex';
-    overlay.style.display = 'block';
+    popup.style.display = 'block';
     profileFormNameInput.value = profileName.textContent;
     profileFormOccupationInput.value = profileOccupation.textContent;
 });
 
 closeModal.addEventListener('click', function (event){
     modal.style.display = 'none';
-    overlay.style.display = 'none';
+    popup.style.display = 'none';
 });
 
 profileForm.addEventListener('submit', function (event) {
     profileName.textContent = profileFormNameInput.value;
     profileOccupation.textContent = profileFormOccupationInput.value;
     modal.style.display = 'none';
-    overlay.style.display = 'none';
+    popup.style.display = 'none';
     event.preventDefault();
 });
 
