99f2f1ec07ccf3c90fcbfe931c5a3e1a099df8fa
RoeJecc <77800695+RoeJecc@users.noreply.github.com>
2021-04-16 17:21:46 -0400

    first session on project 5
    added:
    add button functions
    add button modal
    picture preview
    delete button

--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,118 @@
-// Modal 
+
+// Cards
+
+const initialCards = [
+    {
+      name: "Yosemite Valley",
+      link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+    },
+    {
+      name: "Lake Louise",
+      link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
+    },
+    {
+      name: "Bald Mountains",
+      link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+    },
+    {
+      name: "Latemar",
+      link: "https://code.s3.yandex.net/web-code/latemar.jpg"
+    },
+    {
+      name: "Vanoise National Park",
+      link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
+    },
+    {
+      name: "Lago di Braies",
+      link: "https://code.s3.yandex.net/web-code/lago.jpg"
+    }
+  ];
+
+
+// Declarations
 const openModal = document.querySelector('.profile__edit-button');
-const modal = document.querySelector('.modal');
-const closeModal = document.querySelector('.modal__close-button');
-
-
-// Modal Inputs
+const openAddModal = document.querySelector('.profile__add-button');
+const modal = document.querySelector('.modal_type_profile');
+const addModal = document.querySelector('.modal_type_add-card');
+const previewModal = document.querySelector('.modal_type_preview');
+const closeModal = document.querySelector('.modal__close-button_profile');
+const closeAddModal = document.querySelector('.modal__close-button_add-card');
+const closePreviewModal = document.querySelector('.modal__close-button_preview');
 const profileName = document.querySelector('.profile__name');
 const profileOccupation = document.querySelector('.profile__occupation');
+const imageTitle = document.querySelector('.modal__image-caption');
+
+const elementsBlock = document.querySelector('.elements');
 
 const profileForm = document.forms.profile;
 const profileFormNameInput = profileForm.elements.name;
 const profileFormOccupationInput = profileForm.elements.occupation;
 
-function openPopup() {
-    modal.classList.add('modal_open');
+// Functions
+
+function togglePopup(Popup) {
+  if (!Popup.classList.contains('modal_open')){
     profileFormNameInput.value = profileName.textContent;
     profileFormOccupationInput.value = profileOccupation.textContent;
+}
+    Popup.classList.toggle('modal_open');
     return;
 }
-
-openModal.addEventListener('click',openPopup);
-
-function closePopup() {
-    modal.classList.remove('modal_open');
-    return;
-}
-
-closeModal.addEventListener('click',closePopup);
 
 function submitInfo(event) {
+    event.preventDefault(modal);
     profileName.textContent = profileFormNameInput.value;
     profileOccupation.textContent = profileFormOccupationInput.value;
-    closePopup();
-    event.preventDefault();
-    return;
+    togglePopup(modal);
 }
 
+function createCardElement(card) {
+    const cardTemplate = document.querySelector("#card-template").content.querySelector('.element');
+    const cardElement = cardTemplate.cloneNode(true);
+
+    const cardImage = cardElement.querySelector('.element__image');
+    const cardTitle = cardElement.querySelector('.element__text');
+    const deleteButton = cardElement.querySelector('.element__delete-button');
+
+    cardImage.style.backgroundImage = `url(${card.link})`;
+    cardTitle.textContent = card.name;
+
+    cardImage.addEventListener('click', () => onImagePreview(card));
+
+    deleteButton.addEventListener('click', () => onDeleteButtonClick(cardElement));
+
+    return cardElement;
+}
+
+function renderCard(card, wrapper) {
+    wrapper.append(createCardElement(card));
+}
+
+const onImagePreview = card => {
+    const previewImage = previewModal.querySelector('.modal__image');
+    previewImage.src = card.link;
+    imageTitle.textContent = card.name;
+    togglePopup(previewModal);
+}
+
+const onDeleteButtonClick = cardElement => {
+    elementsBlock.removeChild(cardElement);
+}
+
+// Event Handlers
+
+openModal.addEventListener('click', () => togglePopup(modal));
+
+openAddModal.addEventListener('click', () => togglePopup(addModal));
+
+closeModal.addEventListener('click', () => togglePopup(modal));
+
+closeAddModal.addEventListener('click', () => togglePopup(addModal));
+
+closePreviewModal.addEventListener('click', () => togglePopup(previewModal));
+
 profileForm.addEventListener('submit',submitInfo);
+
+initialCards.forEach(card => renderCard(card, elementsBlock));
+    
+
